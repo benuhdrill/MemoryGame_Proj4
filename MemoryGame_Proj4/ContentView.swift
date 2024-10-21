c1bfb1edd0c4905c8b6e13f0dcd471b0ada9b453
benuhdrill <bgmach2023@fau.edu>
2024-10-21 15:05:21 -0400

    Initial Commit

---
+++ MemoryGame_Proj4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoryGame_Proj4
+//
+//  Created by Ben Gmach on 10/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoryGame_Proj4/MemoryGame_Proj4App.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGame_Proj4App.swift
+//  MemoryGame_Proj4
+//
+//  Created by Ben Gmach on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGame_Proj4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
