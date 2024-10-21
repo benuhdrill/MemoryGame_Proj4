b79f7ef5aa9b04231e1e8df9763f01a2b9cd1a85
benuhdrill <bgmach2023@fau.edu>
2024-10-21 15:08:43 -0400

    Added extra animations

---
+++ MemoryGame_Proj4/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  MemoryGame_Proj4
+//
+//  Created by Ben Gmach on 10/21/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ MemoryGame_Proj4/FlashCardApp.swift
@@ -0,0 +1,18 @@
+//
+//  FlashCardApp.swift
+//  MemoryGame_Proj4
+//
+//  Created by Ben Gmach on 10/21/24.
+//
+
+import SwiftUI
+
+struct FlashCardApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlashCardApp()
+}
